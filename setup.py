e17664feb4ce617079d446c566ff45b9603487ac
Sumeet Padavala <sumeet.padavala@gmail.com>
2019-04-09 22:23:21 +0530

    Add more dependencies to the setup file

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setupdict = {
     "version": "0.0.1",
     "packages": find_packages(),
     "include_package_data": True,
-    "install_requires": ["websockets", "bson"],
+    "install_requires": ["websockets", "bson", 'colorlog', 'numpy', 'pandas'],
 }
 if __name__ == "__main__":
     setup(**setupdict)
