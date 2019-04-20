216f7d85905a4a48497aae8df4b6b2d4eb518fa1
yagamiash <ashobot@gmail.com>
2019-04-20 23:44:38 +0530

    Changed exec.py to work with numpy arrays instead of file paths

--- sync/server/AdaptiveST/exec.py
+++ sync/server/AdaptiveST/exec.py
@@ -21,16 +21,15 @@ import net
 from function import adaptive_instance_normalization
 from function import coral
 from sampler import InfiniteSamplerWrapper
+import numpy as np 
 
 
 
 class RealTimeArbitararyNstWithAdaIn:
 	''' Real Time Arbitrary Style Transfer with Adaptive Instance Normalization '''
 
-	def __init__(self,content_imgs=None,style_imgs=None,content_size=512,style_size=512,output_img=None,alpha=1.0,preserve_color=True,):
+	def __init__(self,content_size=512,style_size=512,output_img=None,alpha=1.0,preserve_color=True,):
 
-		self.content_imgs = content_imgs	# list of content image paths
-		self.style_imgs = style_imgs		# list of Style image paths
 		self.content_size = content_size	# New (minimum) size for the style image,keeping the original size if set to 0
 		self.style_size = style_size		# New (minimum) size for the style image,keeping the original size if set to 0
 		self.output_img = output_img		# Output image path
@@ -65,36 +64,40 @@ class RealTimeArbitararyNstWithAdaIn:
 		transform = transforms.Compose(transform_list)
 		return transform
 
-	def run(self,alpha=1.0):
-		''' Style Transfer Algorithm'''
+	def run(self,content_img,style_img,alpha=1.0):
+		
+		''' Style Transfer Algorithm.Takes Style Image (numpy array with shape (h,w,3)) & Content Image
+			with shape (numpy array with shape (h,w,3)) & return output image as a numpy array of shape (3,h,h)
+		 '''
 		
 		content_tf = self.test_transform(self.content_size,self.crop)	
 		style_tf = self.test_transform(self.style_size,self.crop)
 
-		for content_path in self.content_imgs:
-			for style_path in self.style_imgs:
-				content = content_tf(Image.open(content_path))
-				style = style_tf(Image.open(style_path))
-				if self.preserve_color:
-					style = coral(style, content)
-				style = style.to(self.device).unsqueeze(0)
-				content = content.to(self.device).unsqueeze(0)
-				with torch.no_grad():
-					content_f = self.vgg(content)
-					style_f = self.vgg(style)
-					feat = adaptive_instance_normalization(content_f, style_f)
-					feat = feat * alpha + content_f * (1 - alpha)
-					output = self.decoder(feat)
-				output = output.cpu()
-				output_name = '{:s}/{:s}_stylized_{:s}{:s}'.format(self.output_img, splitext(basename(content_path))[0],splitext(basename(style_path))[0],'.jpg')
-				save_image(output, output_name)	
-				return(output.numpy()[0]) #shape (3, 512, 512)
+		content = content_tf(Image.fromarray(np.uint8(content_img)))
+		style = style_tf(Image.fromarray(np.uint8(style_img)))
+
+		if self.preserve_color:
+			style = coral(style, content)
+		style = style.to(self.device).unsqueeze(0)
+		content = content.to(self.device).unsqueeze(0)
+		with torch.no_grad():
+			content_f = self.vgg(content)
+			style_f = self.vgg(style)
+			feat = adaptive_instance_normalization(content_f, style_f)
+			feat = feat * alpha + content_f * (1 - alpha)
+			output = self.decoder(feat)
+		output = output.cpu()
+		#output_name = '{:s}/{:s}_stylized_{:s}{:s}'.format(self.output_img, splitext(basename('content_img'))[0],splitext(basename('style_img'))[0],'.jpg')
+		#save_image(output, output_name)	
+		return(output.numpy()[0]) #shape (3, 512, 512)
 	
 
 # Runnning Testing & Training
 
 
-adain=RealTimeArbitararyNstWithAdaIn(content_imgs=['/home/ashik/Desktop/hbp.jpg'],
-	style_imgs=['/home/ashik/Desktop/jap.jpg'],
-	output_img='/home/ashik/Desktop/',preserve_color=False)
-adain.run()
+adain=RealTimeArbitararyNstWithAdaIn(output_img='/home/ashik/Desktop/',preserve_color=False)
+
+content_img = np.asarray(Image.open('/home/ashik/Desktop/hbp.jpg'))
+style_img =  np.asarray(Image.open('/home/ashik/Desktop/jap.jpg'))
+print(style_img.shape)
+res=adain.run(content_img,style_img)
