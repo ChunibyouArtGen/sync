d35ed7ebdcaa4fbd7544bdc384f436d0e92c8c0b
Sumeet Padavala <sumeet.padavala@gmail.com>
2019-04-22 22:41:15 +0530

    Add test code

---
+++ sync/server/compute/models/adaptive_st/__main__.py
@@ -0,0 +1,17 @@
+from sync.server.compute.models.adaptive_st import AdaInModel
+from skimage.io import imread, imsave
+from skimage import img_as_ubyte
+import numpy as np
+
+adain=AdaInModel(output_img='/home/ashik/Desktop/',preserve_color=False)
+
+content_img = imread('~/Pictures/content.jpg')
+style_img =  imread('~/Pictures/style.jpg')
+print("Content shape:{}, dtype {}".format(content_img.shape, content_img.dtype))
+print("Style shape:{}, dtype {}".format(style_img.shape, style_img.dtype))
+print("Computing...")
+res = adain.run({'content': content_img,'style':style_img})
+print("Done!")
+print("Result shape:{}, dtype {}".format(res.shape, res.dtype))
+
+imsave("~/Pictures/output.png",res)
